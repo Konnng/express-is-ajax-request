3251850f7403b5a120e79dcbcc59e3e1bef73be8
Konnng <falecom@konnng.com>
2016-11-13 22:09:09 -0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+/*!
+ * express-is-ajax-request
+ * Copyright(c) 2016 Konnng <falecom@konnng.com>
+ * MIT Licensed
+ */
+
+module.exports = function (req, res, next) {
+
+	req.isAjaxRequest = function() {
+		return req.xhr || /json/i.test(req.headers.accept);
+	};
+
+	next();
+};
